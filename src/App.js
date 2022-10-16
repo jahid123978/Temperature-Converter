490530ebda67a32c2f5a288d49ddc83d7925d184
jahid <jahid@workasaserivce.ai>
2022-10-17 03:57:18 +0600

    seond functionality

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
    const [temperatureName, setTemperatureName] = useState([]);
    const [temperature, setTemperature] = useState("");
    const [firstTemperature, setFirstTemperature] = useState(0);
-   const [currency2, setCurrency2] = useState("");
+   const [temperature2, setTemperature2] = useState("");
    const [secondTemperature2, setSecondTemperature2] = useState(0);
   useEffect(()=>{
     fetch("http://localhost:2000/temperature")
@@ -17,7 +17,33 @@ function App() {
     })
   },[])
  
+  function handleSelect1(e){
+    setTemperature(e.target.value);
+    if(e.target.value === temperature2){
+      setSecondTemperature2(firstTemperature);
+    }
+    else if(e.target.value === "celsius" && temperature2 === "faherheit"){
+      setSecondTemperature2(((9*Number(firstTemperature)+160)/5).toFixed(2));
+    }
+    else if(e.target.value === "faherheit" && temperature2 === "celsius"){
+      setSecondTemperature2((((Number(firstTemperature)-32)/9)*5).toFixed(2));
+    }
+    else if(e.target.value === "kalvin" && temperature2 === "celsius"){
+      setSecondTemperature2((Number(firstTemperature)-273).toFixed(2));
+    }
+    else if(e.target.value === "celsius" && temperature2 === "kalvin"){
+      setSecondTemperature2((Number(firstTemperature)+273).toFixed(2));
+    }
+    else if(e.target.value === "faherheit" && temperature2 === "kalvin"){
+      setSecondTemperature2(((Number(firstTemperature)*5 + 2297)/9).toFixed(2));
+    }
+    else if(e.target.value === "kalvin" && temperature2 === "faherheit"){
+      setSecondTemperature2((((Number(firstTemperature) - 273)/5)*9+32).toFixed(2));
+    }
+    
+  }
   function handleSelect2(e){
+    setTemperature2(e.target.value);
     if(e.target.value === temperature){
       setSecondTemperature2(firstTemperature);
     }
@@ -40,7 +66,7 @@ function App() {
       setSecondTemperature2((((Number(firstTemperature) - 273)/5)*9+32).toFixed(2));
     }
     
-
+ 
 
   
   }
@@ -52,7 +78,7 @@ function App() {
      <div className="container">
       <h2>Currency Converter</h2>
      <InputGroup className="mt-5 mb-3">
-     <Form.Select aria-label="Default select example" onChange={(event)=>(setTemperature(event.target.value))}>
+     <Form.Select aria-label="Default select example" onChange={handleSelect1}>
       <option>Select the currency</option>
       {temperatureName.map(c=><option key={c._id} value={c.temp}>{c.temp}</option>)}
     </Form.Select>
